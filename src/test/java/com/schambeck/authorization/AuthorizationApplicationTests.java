56a3bc28284dc094de4e7333a7794345f6deadae
Marcelo Schambeck <mschambeck@gmail.com>
2022-08-20 00:51:43 -0300

    Tests with H2 instead of PostgreSQL

--- src/test/java/com/schambeck/authorization/AuthorizationApplicationTests.java
+++ src/test/java/com/schambeck/authorization/AuthorizationApplicationTests.java
@@ -2,8 +2,10 @@ package com.schambeck.authorization;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class AuthorizationApplicationTests {
 
 	@Test
